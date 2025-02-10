d8c1728ee84d0f07a33a16055b31eedb18a04058
PedroH2440 <guilhermeoberdam973@gmail.com>
2025-02-10 20:26:13 -0300

    aula10/02

--- index.php
+++ index.php
@@ -5,9 +5,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>portifolio</title>
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" width= "150"/>
+
+
+            </div>
+
+            <ul>
+
+            <li>home</li>
+            <li>serviços</li>
+            <li>produtos</li>
+            <li>sobe</li>
+            <li>contatos</li>
+
+
+            </ul>
+
+        </nav>
+
+        <div class="titles">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci inventore illo accusamus eveniet natus quos labore soluta dignissimos fugiat, aut velit quo aliquam nobis dicta alias rem a quia. Molestiae.</p>
+
+        </div>
+
+        <div class="socialIcons">
+
+            <a href="Twitter"></a>
+            <a href="Facebook"></a>
+            <a href="Linkedin"></a>
+            <a href="Instragram"></a>
+            
+        </div>
+
+
+    </header>
+
+
+</div>
     
 </body>
 </html>
