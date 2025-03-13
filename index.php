2061eb601c0bb40d141958b71cd705b7ed1459e6
PedroH2440 <guilhermeoberdam973@gmail.com>
2025-03-13 20:37:11 -0300

    aula13/03/25

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <title>portifolio</title>
 
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css"/>
 
- 
 </head>
 <body>
 
@@ -19,49 +19,93 @@
 
             <div class="logo">
                 <img src="imagens/logo.png" alt="LOGO" />
-
-
             </div>
 
             <ul>
-
-            <li> <a href="#">Home</a></li>
-            <li> <a href="#">Serviços</a></li>
-            <li> <a href="#">Produtos</a></li>
-            <li> <a href="#">Portifolio</a></li>
-            <li> <a href="#">Sobre</a></li>
-            <li> <a href="#">Contatos</a></li>
-
-
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Portifólio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
             </ul>
 
         </nav>
-
-        <div class="titles">
-
+        
+        <div class="titles"> <!-- ========================= -->
             <div class="container-titles">
-
-                <h1 class="titulo">Entregando serviços de qualidade</h1>
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
                 <h1 class="subtitulo">Soluções em TI</h1>
-                <p class="paragrafo">Lorem ipsum dolor sit amet     consectetur, adipisicing elit. </p>
 
-            </div>  
-        </div>
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
+                
+            </div>
 
-        <div class="socialIcons">
-
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
-            <a href="#">Linkedin</a>
-            <a href="#">Instragram</a>
-            
-        </div>
-
-
+            <div class="socialIcons">
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div> <!-- ========================== -->
+        
     </header>
 
+    <!-- ========================================================= -->
+
+    <main id="corpo">
+
+<section class="servicos">
+
+        <article class="servico">
+            <img src="imagens/service-design.svg" alt="Design">
+            <h3 class="title-servico">Desenvolvimento WEB</h3>
+            <p class="text-servico">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis, voluptas error impedit consectetur vitae.</p>
+        </article>
+        <article class="servico">
+            <img src="imagens/service-hosting.svg" alt="Hosting">
+            <h3 class="title-hosting">WEB Hosting</h3>
+            <p class="text-servico">Possimus vel temporibus deleniti repudiandae in similique unde necessitatibus odit corporis nihil soluta, ad repellendus?</p>
+        </article>
+        <article class="servico">
+              <img src="imagens/service-social.svg" alt="Social Media">
+             <h3 class="title-servico">Mídias Sociais</h3>
+             <p class="text-servico">Earum mollitia a, quaerat magni dignissimos, sint velit libero minus magnam saepe perferendis aspernatur? Libero!</p>
+        </article>
+        <article class="servico">
+            <img src="imagens/service-seo.svg" alt="SEO">
+            <h3 class="title-servico">Aplicações SEO</h3>
+            <p class="text-servico">Maiores aliquid sequi expedita. Rerum itaque nihil maxime at explicabo similique recusandae saepe magnam a!</p>
+        </article>
+        <article class="servico">
+            <img src="imagens/service-cloud.svg" alt="Cloud">
+            <h3 class="title-servico">Computação em Nuvem</h3>
+            <p class="text-servico">Provident velit fuga consequuntur quae, iste accusamus repellendus, officiis quibusdam, odio doloribus voluptates iusto perspiciatis.</p>
+        </article>
+         <article class="servico">
+            <img src="imagens/service-secure.svg" alt="Security">
+            <h3 class="title-servico">Segurança de Dados</h3>
+             <p class="text-servico">Expedita illo fugit error in quidem qui, architecto eligendi quae, perspiciatis modi non ab aliquid.</p>
+        </article>
+
+    </section>
+
+        <section class="portifolio">
+
+
+
+        </section>
+
+        <section class="sobre">
+
+
+
+        </section>
+
+
+    </main>
 
 </div>
-    
+
 </body>
 </html>
